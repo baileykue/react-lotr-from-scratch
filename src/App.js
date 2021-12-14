b6889766847585e56f049ff4e826ccad92ce5395
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-12-14 15:52:13 -0800

    properly linked films files to app js, got the first route working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import './App.css';
+import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
+import Films from './views/Films/Films';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <header>
+          <NavLink exact to="/films" data-testid="film-link">
+            Films
+          </NavLink>
+        </header>
+
+        <Switch>
+          <Route path="/films">
+            <Films />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
