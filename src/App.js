87d7f665fb36ff82c2a652c3aac1852e9f51d50b
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-12-14 16:01:19 -0800

    added books use effect and fetch functions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
 import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
 import Films from './views/Films/Films';
+import Main from './views/Main/Main';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <header>
-          <NavLink exact to="/films" data-testid="film-link">
+          <NavLink exact to="/" data-testid="home-link">
+            Home
+          </NavLink>
+          <NavLink to="/films" data-testid="film-link">
             Films
           </NavLink>
         </header>
 
         <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
           <Route path="/films">
             <Films />
           </Route>
--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Main() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>This is the Home Page</h1>
+    </div>
+  );
 }
