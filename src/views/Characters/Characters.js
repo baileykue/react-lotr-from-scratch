4900628ed7f098d71ddc2b0ae3a0827b1155e6ab
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-12-14 17:59:26 -0800

    added font and icon, started basic styling

--- src/views/Characters/Characters.js
+++ src/views/Characters/Characters.js
@@ -4,6 +4,8 @@ import { fetchCharacters } from '../../services/characters';
 
 export default function Characters() {
   const [characters, setCharacters] = useState([]);
+  const [query, setQuery] = useState('');
+  const [race, setRace] = useState('All');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,6 +18,17 @@ export default function Characters() {
   return (
     <div>
       <h1>Characters</h1>
+      <input></input>
+      <select>
+        <option value="All">All</option>
+        <option value="Dwarf">Dwarf</option>
+        <option value="Elf">Elf</option>
+        <option value="Hobbit">Hobbit</option>
+        <option value="Human">Human</option>
+        <option value="Maiar">Maiar</option>
+        <option value="Orc">Orc</option>
+      </select>
+      <button>Search</button>
       <CharactersList characters={characters} />
     </div>
   );
